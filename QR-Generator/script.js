f271d66505ab71548e7fa90e4adfe0d72823e11e
Rahul Bhatt <72143946+rahul-bhatt43@users.noreply.github.com>
2023-07-23 23:26:42 +0530

    changes made

--- QR-Generator/script.js
+++
@@ -1,7 +0,0 @@
-var qrdata = document.getElementById('qr-data');
-var qrcode = new QRCode(document.getElementById('qrcode'));
-
-function generateQR(){
-    var data = qrdata.value;
-    qrcode.makeCode(data);
-}
